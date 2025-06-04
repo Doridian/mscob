13f83dd9580f8db10c610cb521fc7ac80e999d09
Doridian <git@doridian.net>
2025-06-03 18:36:54 -0700

    rename

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@ set -ex
 export COBCPY=/usr/share/open-cobol-esql/copy
 export COB_LDFLAGS=-Wl,--no-as-needed
 
-rm -f nssrv
+rm -f NOTIFICATION-SERVER SWITCHBOARD-SERVER
 
-cobc -O2 -x nssrv.cbl -o nssrv
-#socat TCP-LISTEN:1863,reuseaddr,fork EXEC:"./nssrv"
-./nssrv
+cobc -O2 -x NOTIFICATION-SERVER.CBL -o NOTIFICATION-SERVER
+#socat TCP-LISTEN:1863,reuseaddr,fork EXEC:"./NOTIFICATION-SERVER"
+./NOTIFICATION-SERVER
