9c444bcb0f544cf799ff21381d8d46f4f1b5de45
Doridian <git@doridian.net>
2025-06-05 13:25:52 -0700

    fix

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -ex
 export COBCPY=/usr/share/open-cobol-esql/copy
 export COB_LDFLAGS=-Wl,--no-as-needed
 
-rm -f NOTIFICATION-SERVER SWITCHBOARD-SERVER
+rm -f notification-server switchboard-server
 
 
 cobc -O2 -x notification-server.cbl files.c -o notification-server
